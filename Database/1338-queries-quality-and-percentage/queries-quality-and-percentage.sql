a56e11930db8f56fde92b582ba351d471a301c85
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-01-24 23:55:15 -0800

    Update queries-quality-and-percentage.sql

--- Database/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ Database/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,10 +1,5 @@
-# Write your MySQL query statement below
-
-# new columns: quality, 
 select query_name,
-    --    position, 
-    --    rating,
        ROUND(AVG(rating / position), 2) as quality,
-       ROUND(SUM(case when rating < 3 then 1 else 0 end) / count(*) * 100, 2) as poor_query_percentage
+       ROUND(AVG(rating < 3) * 100, 2) as poor_query_percentage
 from queries
 group by query_name
