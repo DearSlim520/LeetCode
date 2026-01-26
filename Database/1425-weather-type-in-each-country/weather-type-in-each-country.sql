057fa75a75d8d4c15fe7ecdd4fda8ff871211ec5
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-01-25 23:13:44 -0800

    Time: 645 ms (13.08%) | Memory: 0B (100.00%) - LeetSync

---
+++ Database/1425-weather-type-in-each-country/weather-type-in-each-country.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+
+# deal with weather: avg
+# inner join for country name
+
+with cte_country_avg as (
+    select country_id,
+        date_format(day, '%Y-%m') as month,
+        avg(weather_state) as avg_temp
+    from weather
+    where date_format(day, '%Y-%m') = '2019-11'
+    group by 1, 2
+    )
+select c.country_name,
+       case when a.avg_temp <= 15 then 'Cold'
+            when a.avg_temp >= 25 then 'Hot'
+            else 'Warm' end                  as weather_type
+from Countries c
+    inner join cte_country_avg a
+        on c.country_id = a.country_id
