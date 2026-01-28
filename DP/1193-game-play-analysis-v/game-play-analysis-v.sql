558acdde58aac9de1d760b2007bd78694bff097d
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-01-28 11:09:37 -0800

    Time: 493 ms (68.09%) | Memory: 0B (100.00%) - LeetSync

--- DP/1193-game-play-analysis-v/game-play-analysis-v.sql
+++ DP/1193-game-play-analysis-v/game-play-analysis-v.sql
@@ -1,30 +1,25 @@
 # Write your MySQL query statement below
 
 # by player + 1st event_date
-with cte_rn as (
+with cte_day1 as (
         select player_id, 
-               event_date,
-               row_number() over (partition by player_id order by event_date) as rn
+               min(event_date) as day1
         from activity
-    ),
-    cte_day1_n_2 as (
-        select player_id,
-               MAX(case when rn = 1 then event_date end) as day1,
-               MAX(case when rn = 2 then event_date else null end) as day2
-        from cte_rn
-        where rn <= 2
         group by player_id
     ),
-    cte_retention_by_player as(
-        select player_id,
-               day1,
-               day2,
-               if(day2 is not null and day2 - day1 = 1, 1, 0) as retention
-        from cte_day1_n_2    
+    cte_day1_n_2 as (
+        select d.player_id,
+               d.day1,
+               a.event_date as day2,
+               if(a.event_date is not null, 1, 0) as retention
+        from cte_day1 d
+            LEFT JOIN activity a
+                ON d.player_id = a.player_id
+                AND date_add(d.day1, interval 1 day) = a.event_date
     )
 select day1 as install_dt,
        count(player_id) as installs,
        ROUND(sum(retention) / count(player_id), 2) as 'Day1_retention'
-from cte_retention_by_player
+from cte_day1_n_2
 group by 1
 order by 1
