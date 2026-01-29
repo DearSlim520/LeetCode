ef5b52acf72020095fe72b3afa1cb497e243f52c
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-01-28 16:57:40 -0800

    Update report-contiguous-dates.sql
    Combine both approaches

--- Database/1357-report-contiguous-dates/report-contiguous-dates.sql
+++ Database/1357-report-contiguous-dates/report-contiguous-dates.sql
@@ -1,5 +1,33 @@
 # Write your MySQL query statement below
 
+# solution 1: group by sum(is_start)
+with cte_prev as (
+        select 'failed' as status,
+                fail_date as event_date,
+                COALESCE(lag(fail_date) over (order by fail_date), '1900-01-01') as prev_event_date
+        from failed
+        where year(fail_date) = '2019'
+        UNION ALL
+        select 'succeeded' as status,
+                success_date as event_date,
+                COALESCE(lag(success_date) over (order by success_date), '1900-01-01') as prev_event_date
+        from succeeded
+        where year(success_date) = '2019'
+    ),
+    cte_island as (
+        select status,
+               event_date,
+               sum(case when datediff(event_date, prev_event_date) > 1 
+                        then 1 else 0 end) over (order by event_date asc) as sum_start
+        from cte_prev
+    )
+select status as period_state, 
+       min(event_date) as start_date,
+       max(event_date) as end_date
+from cte_island
+group by sum_start
+
+# solution 2: group by diff (date offset - rn)
 with cte_prev as (
         select 'failed' as status,
                 fail_date as event_date,
