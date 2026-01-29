7bb80a92ce24dc012646cf670826a3056997c807
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-01-28 16:34:09 -0800

    Time: 1079 ms (57.98%) | Memory: 0B (100.00%) - LeetSync

---
+++ Database/1357-report-contiguous-dates/report-contiguous-dates.sql
@@ -0,0 +1,34 @@
+# Write your MySQL query statement below
+
+with cte_prev as (
+        select 'failed' as status,
+                fail_date as event_date,
+                COALESCE(lag(fail_date) over (order by fail_date), '1900-01-01') as prev_event_date
+        from failed
+        where year(fail_date) = '2019'
+        UNION ALL
+        select 'succeeded' as status,
+                success_date as event_date,
+                COALESCE(lag(success_date) over (order by success_date), '1900-01-01') as prev_event_date
+        from succeeded
+        where year(success_date) = '2019'
+    ),
+    cte_date_w_flags as (
+        select status,
+               event_date,
+               sum(case when datediff(event_date, prev_event_date) > 1 then 1 else 0 end) over (order by event_date asc) as sum_start,
+               row_number() over (order by event_date) as rn
+        from cte_prev
+    )
+    -- ,
+    -- cte_island as (
+    --     select status, 
+    --            event_date,
+    --            sum(is_start) over (order by event_date asc) as sum_start
+    --     from cte_date_w_flags
+    -- )
+select status as period_state, 
+       min(event_date) as start_date,
+       max(event_date) as end_date
+from cte_date_w_flags
+group by sum_start
