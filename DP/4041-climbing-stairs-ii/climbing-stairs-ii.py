10395cd171c9cefedb9678245a3bd54c6f27ab40
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-02-05 15:10:33 -0800

    Time: 343 ms (78.24%) | Memory: 33.4 MB (47.99%) - LeetSync

--- DP/4041-climbing-stairs-ii/climbing-stairs-ii.py
+++ DP/4041-climbing-stairs-ii/climbing-stairs-ii.py
@@ -2,13 +2,14 @@ class Solution:
     def climbStairs(self, n: int, costs: List[int]) -> int:
         if n == 1:
             return costs[0] + 1
-        if n == 2:
-            return min(4, costs[0] + 2) + costs[1]
-        
+
         dp = [0] * (n + 1)
+        
+        # base case
         dp[0] = 0
-        dp[1] = costs[0] + 1   # 2
-        dp[2] = min(4, dp[1] + 1) + costs[1]   #min(4, 3) + 2 = 5
+        dp[1] = costs[0] + 1
+        if n >= 2:
+            dp[2] = min(4, dp[1] + 1) + costs[1]
 
         for i in range(3, n+1):
             dp[i] = costs[i - 1] + min([9 + dp[i-3], 4 + dp[i-2], 1 + dp[i-1]])
