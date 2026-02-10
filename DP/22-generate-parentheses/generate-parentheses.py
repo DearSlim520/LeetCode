9afea72333e953ff302c7fa2eacd0e6de4f290e3
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-02-09 23:54:05 -0800

    Time: 3 ms (31.91%) | Memory: 19.3 MB (59.08%) - LeetSync

--- DP/22-generate-parentheses/generate-parentheses.py
+++ DP/22-generate-parentheses/generate-parentheses.py
@@ -4,29 +4,24 @@ class Solution:
         self.ans = []
 
     def generateParenthesis(self, n: int) -> List[str]:
-        self.backtrack(n, [])
+        self.backtrack(n, "")
         return self.ans
 
-    def backtrack(self, n: int, track: List[str]):
+    def backtrack(self, n: int, track: str):
+        # if self.seen['('] == n and self.seen[')'] == n:
         if len(track) == n * 2:
-            curStr = ''.join(track)
-            if curStr not in self.ans:
-                self.ans.append(curStr)
+            self.ans.append(track)
             return
         
-        for x in ['(',')']:
-            # pruning:
-            if self.seen[x] == n:
-                continue
-            if x == ')' and self.seen['('] == self.seen[')']:
-                continue
+        # pruning
+        if self.seen['('] < n:
+            self.seen['('] += 1
+            self.backtrack(n, track + '(')
+            self.seen['('] -= 1
 
-            # make choice
-            track.append(str(x))
-            self.seen[x] += 1
-            self.backtrack(n, track)
-            # cancel choice
-            track.pop()
-            self.seen[x] -= 1
+        if self.seen[')'] < self.seen['(']:
+            self.seen[')'] += 1
+            self.backtrack(n, track + ')')
+            self.seen[')'] -= 1
 
         return
