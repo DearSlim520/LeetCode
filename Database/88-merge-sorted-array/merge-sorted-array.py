e9a8397e39457190fd17bcd607df2d78599adc4c
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-02-22 20:18:24 -0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (64.64%) - LeetSync

--- Database/88-merge-sorted-array/merge-sorted-array.py
+++ Database/88-merge-sorted-array/merge-sorted-array.py
@@ -5,30 +5,16 @@ class Solution:
         """
         p1, p2 = m-1, n-1
         p = m + n - 1
-        # while p1 >= 0 and p2 >= 0:
-        #     if nums1[p1] > nums2[p2]:
-        #         nums[p] = nums1[p1]
-        #         p1 -= 1
-        #     else:
-        #         nums[p] = nums2[p2]
-        #         p2 -= 1
-        #     p -= 1
-
-        
-        
-        while p >= 0:
-            if p1 >= 0 and p2 >= 0:
-                if nums1[p1] > nums2[p2]:
-                    nums1[p] = nums1[p1]
-                    p1 -= 1
-                else:
-                    nums1[p] = nums2[p2]
-                    p2 -= 1
-            elif p2 < 0:
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
                 nums1[p] = nums1[p1]
                 p1 -= 1
             else:
                 nums1[p] = nums2[p2]
                 p2 -= 1
             p -= 1
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p -= 1 
+            p2 -= 1 
         return nums1
