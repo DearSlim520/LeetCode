f4f85fa9633e083dd2fed47148a939a11f2a1cdb
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-02-27 15:39:37 -0800

    Time: 7 ms (57.06%) | Memory: 20 MB (87.21%) - LeetSync

--- Array/45-jump-game-ii/jump-game-ii.py
+++ Array/45-jump-game-ii/jump-game-ii.py
@@ -1,17 +1,19 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [n] * n
         if n == 1:
             return 0
 
-        for i in range(n-2, -1, -1):
-            if nums[i] > 0:
-                curr_furthest = i + nums[i]
-                if curr_furthest >= n-1:
-                    dp[i] = 1
-                else:
-                    dp[i] = min(dp[i+1:curr_furthest+1]) + 1
+        step = 0
+        curr_furthest = 0
+        global_furthest = 0
 
-        return dp[0]
-            
+        for i in range(n-1):
+            global_furthest = max(global_furthest, i + nums[i])
+            if i == curr_furthest:
+                step += 1
+                curr_furthest = global_furthest
+            if curr_furthest >= n+1:
+                break
+
+        return step
