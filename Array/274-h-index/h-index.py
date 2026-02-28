b80194d1db2c50effd188b1b08d3a6dd45be589e
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-02-28 09:22:15 -0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (82.20%) - LeetSync

--- Array/274-h-index/h-index.py
+++ Array/274-h-index/h-index.py
@@ -1,11 +1,5 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        # necessarity of sort
-        # sort: [0, 1, 3, 5, 6]
-        # [6, 5, 3, 1, 0]
-
-        # n - i >= nums[i]
-
         citations.sort(reverse = True)
         i = 0
         res = 0
