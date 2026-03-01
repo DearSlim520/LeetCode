b6c17a6f3dc72b6edba5ec45b89820183714978c
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-03-01 14:20:05 -0800

    Time: 8 ms (59.17%) | Memory: 21.1 MB (73.19%) - LeetSync

--- Array/42-trapping-rain-water/trapping-rain-water.py
+++ Array/42-trapping-rain-water/trapping-rain-water.py
@@ -1,28 +1,21 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
-        if n == 1:
-            return 0
-
+        left, right = 0, n-1
+        leftMax = rightMax = 0
         area = 0
-        curArea, curSide = 0, height[0]
-        # left to right
-        for i in range(n):
-            if height[i] > curSide:
-                area += curArea
-                curSide = height[i]
-                curArea = 0
-            else:
-                curArea += curSide - height[i]
 
-        curArea, curSide = 0, height[-1]
-        # right to left
-        for i in range(n-1, -1, -1):
-            if height[i] >= curSide:
-                area += curArea
-                curSide = height[i]
-                curArea = 0
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] < leftMax:
+                    area += leftMax - height[left]
+                else:
+                    leftMax = height[left]
+                left += 1
             else:
-                curArea += curSide - height[i]
-            
+                if height[right] < rightMax:
+                    area += rightMax - height[right]
+                else:
+                    rightMax = height[right]
+                right -= 1
         return area
