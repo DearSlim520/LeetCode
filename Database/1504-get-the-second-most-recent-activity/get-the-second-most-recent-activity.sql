a0e883a7c1e27e1e43f53473cdc2fce8e8033cf3
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-03-06 14:49:51 -0800

    Time: 334 ms (90.99%) | Memory: 0B (100.00%) - LeetSync

--- Database/1504-get-the-second-most-recent-activity/get-the-second-most-recent-activity.sql
+++ Database/1504-get-the-second-most-recent-activity/get-the-second-most-recent-activity.sql
@@ -1,18 +1,11 @@
 # Write your MySQL query statement below
 
 with cte_rank as (
-        select *, row_number() over (partition by username order by startDate desc) as rn
+        select *, 
+               row_number() over (partition by username order by startDate desc) as rn,
+               count(*) over (partition by username) as cnt
         from useractivity
-    ),
-    cte_unique as (
-        select *
-        from useractivity
-        group by username
-        having count(*) = 1
     )
 select username, activity, startDate, endDate
 from cte_rank
-where rn = 2
-UNION ALL
-select username, activity, startDate, endDate
-from cte_unique
+where rn = 2 or cnt = 1
