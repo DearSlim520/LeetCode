4b9ced38e94a82b8e17bebc99cd7852fdd77568c
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-03-08 23:11:56 -0700

    Time: 8 ms (42.00%) | Memory: 22.4 MB (83.17%) - LeetSync

--- Array/56-merge-intervals/merge-intervals.py
+++ Array/56-merge-intervals/merge-intervals.py
@@ -1,18 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        n = len(intervals)
         res = []
         start = 0
-        maxEnd = intervals[0][1]
-        for i in range(n):
-            maxEnd = max(maxEnd, intervals[i][1])
-            # non-overlap
-            if i == n-1 or maxEnd < intervals[i+1][0]:
-                if start == i:
-                    res.append(intervals[i])
-                else:
-                    res.append([intervals[start][0], maxEnd])
-                start = i + 1
-
+        
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= intervals[start][1]:
+                intervals[start][1] = max(intervals[start][1], intervals[i][1])
+            else:
+                res.append(intervals[start])
+                start = i
+        
+        res.append(intervals[start])
         return res
