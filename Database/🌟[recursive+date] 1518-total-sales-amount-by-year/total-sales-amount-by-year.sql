ab78b7640992699657b8623492c3df8ed1c261e4
DearSlim520 <dearslim99@gmail.com>
2026-03-10 20:20:51 -0700

    move folders and several intervals questions

--- Database/🌟[recursive+date] 1518-total-sales-amount-by-year/total-sales-amount-by-year.sql
+++ Database/🌟[recursive+date] 1518-total-sales-amount-by-year/total-sales-amount-by-year.sql
@@ -14,25 +14,19 @@ with recursive cte_year as (
     cte_sales_date as (
         select s.product_id, 
                d.yr as report_year, 
-               greatest(d.year_start, s.period_start) as real_start,
-               least(d.year_end, s.period_end) as real_end,
+               greatest(d.year_start, s.period_start) as effective_start,
+               least(d.year_end, s.period_end) as effective_end,
                s.average_daily_sales
         from cte_dim_date d
-            CROSS JOIN sales s
-    ),
-    cte_calculate as (
-        select product_id,
-               report_year,
-               datediff(real_end, real_start) + 1 as cnt,
-               average_daily_sales
-        from cte_sales_date
-        where real_start <= real_end 
+            INNER JOIN sales s
+                ON d.yr between year(s.period_start) and year(s.period_end)
     )
 select c.product_id,
        p.product_name,
        cast(c.report_year as CHAR) as report_year,
-       c.cnt * c.average_daily_sales as total_amount
-from cte_calculate c
+       (datediff(c.effective_end, c.effective_start) + 1) * c.average_daily_sales as total_amount
+from cte_sales_date c
     left join product p
         on c.product_id = p.product_id
+where c.effective_end >= c.effective_start
 order by c.product_id, c.report_year
