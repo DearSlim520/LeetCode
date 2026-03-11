bcd2d2c64416709331bd2b6f10872813c8bf0736
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-03-10 20:27:08 -0700

    Time: 349 ms (24.74%) | Memory: 0B (100.00%) - LeetSync

--- Array/1578-apples-oranges/apples-oranges.sql
+++ Array/1578-apples-oranges/apples-oranges.sql
@@ -1,12 +1,10 @@
 # Write your MySQL query statement below
 
-with cte_adjust_sign as(
+
     select sale_date,
-        case when fruit = 'apples' then sold_num
-                when fruit = 'oranges' then -1 * sold_num end as sold_num
+        sum(case when fruit = 'apples' then sold_num
+             when fruit = 'oranges' then -1 * sold_num end) as diff
     from sales
-)
-select sale_date, sum(sold_num) as diff
-from cte_adjust_sign
-group by sale_date
+    group by sale_date
 order by sale_date
+
