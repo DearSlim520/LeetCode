8651f8ff01f22f3808f6011e098b5c3ea1b86b19
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-03-14 11:07:59 -0700

    Time: 59 ms (33.47%) | Memory: 20.2 MB (80.11%) - LeetSync

--- Database/224-basic-calculator/basic-calculator.py
+++ Database/224-basic-calculator/basic-calculator.py
@@ -1,34 +1,30 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stk = []
-        curNum = 0
-        subtotal = 0
-        sign = 1
         i, n = 0, len(s)
+        sign = 1
+        curSum, subtotal = 0, 0
+        stk = []
 
         while i < n:
             if s[i] == ' ':
                 i += 1
-
+                continue
             elif s[i].isdigit():
                 while i < n and s[i].isdigit():
-                    curNum = curNum * 10 + int(s[i])
+                    curSum = curSum * 10 + int(s[i])
                     i += 1
-                subtotal += sign * curNum
-                curNum = 0
-
+                subtotal += sign * curSum
+                curSum = 0
             else:
-                if s[i] in ('+','-'):
+                if s[i] in ('+', '-'):
                     sign = 1 if s[i] == '+' else -1
                 elif s[i] == '(':
                     stk.append(subtotal)
                     stk.append(sign)
-                    subtotal = 0
                     sign = 1
+                    subtotal = 0
                 else:
                     subtotal = stk.pop() * subtotal + stk.pop()
                 i += 1
-            
-
+        
         return subtotal
-
