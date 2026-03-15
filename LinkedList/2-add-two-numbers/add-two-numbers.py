d4b4070ef6f37f62bd51d3e0b410e98828de118a
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-03-14 18:05:39 -0700

    Time: 3 ms (65.68%) | Memory: 19.4 MB (71.77%) - LeetSync

--- LinkedList/2-add-two-numbers/add-two-numbers.py
+++ LinkedList/2-add-two-numbers/add-two-numbers.py
@@ -5,19 +5,18 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        p1, p2 = l1, l2
         carry = 0
         dummy = ListNode(0)
         cur = dummy
         
-        while p1 or p2 or carry:
+        while l1 or l2 or carry:
             curVal = carry
-            if p1:
-                curVal += p1.val
-                p1 = p1.next
-            if p2:
-                curVal += p2.val
-                p2 = p2.next
+            if l1:
+                curVal += l1.val
+                l1 = l1.next
+            if l2:
+                curVal += l2.val
+                l2 = l2.next
             carry = 1 if curVal > 9 else 0
             cur.next = ListNode(curVal % 10)
             cur = cur.next
