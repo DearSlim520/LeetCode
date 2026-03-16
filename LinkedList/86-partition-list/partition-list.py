0f02bbd8c990c13c29b9212510c03029add1bfe9
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-03-16 10:57:30 -0700

    Time: 1 ms (14.82%) | Memory: 19.3 MB (91.29%) - LeetSync

---
+++ LinkedList/86-partition-list/partition-list.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        dummy = ListNode(-101)
+        dummy.next = head
+        left = dummy
+
+        # find the spot to insert
+        while left.next and left.next.val < x:
+            left = left.next
+        right = left.next
+        if not right:
+            return head
+
+        print(left.val, right.val)
+        # find targets
+        prev = right
+        while prev and prev.next:
+            print(prev.val)
+            if prev.next.val < x:
+                print('found less')
+                prev, left = self.switch(prev, left, right)
+            else:
+                prev = prev.next
+
+        return dummy.next
+        
+
+    def switch(self, prev: ListNode, left: ListNode, right: ListNode) -> ListNode:
+        target = prev.next
+        prev.next = prev.next.next
+        left.next = target
+        target.next = right
+
+        return prev, left.next
