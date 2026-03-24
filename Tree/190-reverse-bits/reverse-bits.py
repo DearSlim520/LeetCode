ab1394310875d8dbc2ed3872b527c868d7687187
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-03-23 22:53:57 -0700

    Time: 46 ms (58.29%) | Memory: 19.1 MB (89.98%) - LeetSync

--- Tree/190-reverse-bits/reverse-bits.py
+++ Tree/190-reverse-bits/reverse-bits.py
@@ -1,7 +1,7 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         result = 0
-        for i in range(32):
+        for _ in range(32):
             # 把 n 的最低位取出，放到 result 的最高位
             result = (result << 1) | (n & 1)
             n >>= 1                    # n 右移一位
