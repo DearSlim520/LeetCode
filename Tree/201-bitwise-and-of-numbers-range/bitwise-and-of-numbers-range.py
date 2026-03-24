a36d5158c4c78d9033ed82c1080a30560b812ca6
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-03-24 00:47:17 -0700

    Time: 7 ms (39.19%) | Memory: 19.1 MB (93.77%) - LeetSync

---
+++ Tree/201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        # 找到 left 和 right 的最高不同位，然后把 right 中该位及之后的所有位清零
+        if left == 0:
+            return 0
+
+        shift = 0
+        while left != right:
+            left >>= 1
+            right >>= 1
+            shift += 1
+        
+        return right << shift
