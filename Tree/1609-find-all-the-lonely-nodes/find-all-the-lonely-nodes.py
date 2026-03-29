532631810c81cc04c7ee1d264137ca0489d32781
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-03-28 22:54:09 -0700

    Time: 2 ms (57.87%) | Memory: 20 MB (26.40%) - LeetSync

---
+++ Tree/1609-find-all-the-lonely-nodes/find-all-the-lonely-nodes.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getLonelyNodes(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def dfs(node, hasSibling) -> int:
+            nonlocal res
+            if not node:
+                return
+            if hasSibling == False:
+                res.append(node.val)
+            dfs(node.left, node.left is not None and node.right is not None)
+            dfs(node.right, node.left is not None and node.right is not None)
+            return
+
+        dfs(root, True)
+        return res
