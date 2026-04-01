50fdd6c66116d54e7c5fc03cd2933971e07d7738
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-04-01 08:59:12 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (40.59%) - LeetSync

---
+++ Tree/1011-flip-binary-tree-to-match-preorder-traversal/flip-binary-tree-to-match-preorder-traversal.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flipMatchVoyage(self, root: Optional[TreeNode], voyage: List[int]) -> List[int]:
+        self.res = []
+        self.index = 0
+        def dfs(node) -> bool:
+            if not node:
+                return True
+            if node.val != voyage[self.index]:
+                return False
+            self.index += 1
+            # do not flip
+            if node.left and node.left.val != voyage[self.index]:
+                self.res.append(node.val)
+                return dfs(node.right) and dfs(node.left)
+            # flip
+            else:
+                return dfs(node.left) and dfs(node.right)
+            return False
+        
+        if dfs(root) and self.index == len(voyage):
+            return self.res
+        return [-1]
+
