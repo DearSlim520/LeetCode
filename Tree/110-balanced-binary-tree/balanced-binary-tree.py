ce27c32a3e9ecaae147931cd4fb1e132e9191e6b
Slim <50854083+DearSlim520@users.noreply.github.com>
2026-04-03 15:47:57 -0700

    Time: 0 ms (100.00%) | Memory: 20.8 MB (8.74%) - LeetSync

---
+++ Tree/110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.maxDiff = 0
+        def dfs(node, depth):
+            if not node:
+                return depth - 1
+            l = dfs(node.left, depth + 1)
+            r = dfs(node.right, depth + 1)
+            self.maxDiff = max(self.maxDiff, abs(l - r))
+            return max(l, r)
+
+        dfs(root, 1)
+        return self.maxDiff <= 1
