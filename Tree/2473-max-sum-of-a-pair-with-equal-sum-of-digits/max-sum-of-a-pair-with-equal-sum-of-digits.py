dfd39ed5bb3117742aa88e4e3058a2908b2d3e5a
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-04-02 23:17:26 -0700

    Time: 592 ms (5.05%) | Memory: 33.4 MB (88.18%) - LeetSync

--- Tree/2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ Tree/2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -3,13 +3,10 @@ class Solution:
         seen = {}
         res = 0
         nums.sort(reverse = True)
-        print(nums)
         for num in nums:
             cur = sum(int(d) for d in str(num))
             if cur in seen:
                 res = max(res, num + max(seen[cur]))
-                # if num not in seen[cur]:
-                #     seen[cur].append(num)
             else:
                 seen[cur] = [num]
 
