bd555c0619bc9cccb1da4070f7d4d3befd86c688
Jiajie Qin <50854083+DearSlim520@users.noreply.github.com>
2026-04-02 23:19:41 -0700

    Time: 488 ms (17.73%) | Memory: 33.4 MB (73.68%) - LeetSync

--- Tree/2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ Tree/2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -2,12 +2,14 @@ class Solution:
     def maximumSum(self, nums: List[int]) -> int:
         seen = {}
         res = 0
-        nums.sort(reverse = True)
+
         for num in nums:
             cur = sum(int(d) for d in str(num))
             if cur in seen:
-                res = max(res, num + max(seen[cur]))
+                res = max(res, num + seen[cur])
+                if num > seen[cur]:
+                    seen[cur] = num
             else:
-                seen[cur] = [num]
+                seen[cur] = num
 
         return res if res > 0 else -1
