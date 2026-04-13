3ff4e217fac45766b1df7f5b0ab657e44ec7ffd2
Slim <50854083+DearSlim520@users.noreply.github.com>
2026-04-13 00:03:56 -0700

    Time: 0 ms (100.00%) | Memory: 20.3 MB (94.66%) - LeetSync

--- Tree/207-course-schedule/course-schedule.py
+++ Tree/207-course-schedule/course-schedule.py
@@ -1,29 +1,18 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # 这题本质是判断prerequisites里面是否存在环： 是，False； 否，True
-        # create graph
         graph = [[] for _ in range(numCourses)]
-        for a, b in prerequisites:
+        preCoureses = [0] * numCourses
+        for a,b in prerequisites:
             graph[b].append(a)
+            preCoureses[a] += 1
 
-        visited = [0] * numCourses # 0 unvisited, 1 visited, 2 complete
-        def dfs(course) -> bool:
-            # exits
-            if visited[course] == 1:
-                return False
-            if visited[course] == 2:
-                return True
-            
-            visited[course] = 1
-
-            for next_course in graph[course]:
-                if not dfs(next_course):
-                    return False
-            visited[course] = 2
-            return True
-
-        for i in range(numCourses):
-            if visited[i] == 0:
-                if not dfs(i):
-                    return False
-        return True
+        completed = [0] * numCourses
+        q = deque([i for i in range(numCourses) if preCoureses[i] == 0])
+        while q:
+            cur = q.popleft()
+            completed[cur] = 1
+            for next_course in graph[cur]:
+                preCoureses[next_course] -= 1
+                if preCoureses[next_course] == 0:
+                    q.append(next_course)
+        return True if sum(completed) == numCourses else False
