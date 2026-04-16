39516303af4a3f991386993112e5be329a17471b
Slim <50854083+DearSlim520@users.noreply.github.com>
2026-04-15 23:09:15 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (29.08%) - LeetSync

--- Tree/66-plus-one/plus-one.py
+++ Tree/66-plus-one/plus-one.py
@@ -2,15 +2,13 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         i = len(digits) - 1
         carry = 1
-        while i >= 0:
+        while i >= 0 and carry:
             if digits[i] < 9:
                 digits[i] += carry
                 carry = 0
-                break
             else:
-                if carry == 0:
-                    break
                 digits[i] = 0
+                carry = 1
             i -= 1
         if carry:
             digits.insert(0, 1)
