0f5962001ada9b2649bc07d98c7c2c92c1cdd670
Slim <50854083+DearSlim520@users.noreply.github.com>
2026-04-22 23:48:01 -0700

    Time: 5 ms (55.90%) | Memory: 20.3 MB (81.87%) - LeetSync

--- Tree/207-course-schedule/course-schedule.py
+++ Tree/207-course-schedule/course-schedule.py
@@ -1,18 +1,19 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = [[] for _ in range(numCourses)]
-        preCoureses = [0] * numCourses
-        for a,b in prerequisites:
-            graph[b].append(a)
-            preCoureses[a] += 1
-
-        completed = [0] * numCourses
-        q = deque([i for i in range(numCourses) if preCoureses[i] == 0])
+        # build graph
+        preCourses = [0] * numCourses
+        nextGraph = [[] for _ in range(numCourses)]
+        for a, b in prerequisites:
+            nextGraph[b].append(a)
+            preCourses[a] += 1
+            
+        complete = [0] * numCourses
+        q = deque([i for i in range(numCourses) if preCourses[i] == 0])
         while q:
             cur = q.popleft()
-            completed[cur] = 1
-            for next_course in graph[cur]:
-                preCoureses[next_course] -= 1
-                if preCoureses[next_course] == 0:
-                    q.append(next_course)
-        return True if sum(completed) == numCourses else False
+            complete[cur] = 1
+            for nextCourse in nextGraph[cur]:
+                preCourses[nextCourse] -= 1
+                if preCourses[nextCourse] == 0:
+                    q.append(nextCourse)
+        return True if sum(complete) == numCourses else False
