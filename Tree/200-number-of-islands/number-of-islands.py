df72be2c48ce211c555062b31031818fa544db91
Slim <50854083+DearSlim520@users.noreply.github.com>
2026-04-27 21:18:34 -0700

    Time: 238 ms (75.05%) | Memory: 21.4 MB (91.73%) - LeetSync

--- Tree/200-number-of-islands/number-of-islands.py
+++ Tree/200-number-of-islands/number-of-islands.py
@@ -1,23 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        def findborder(grid, i, j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
+                return
+            if grid[i][j] == "0":
+                return
+            grid[i][j] = "0"
+            findborder(grid, i - 1, j)
+            findborder(grid, i + 1, j)
+            findborder(grid, i, j - 1)
+            findborder(grid, i, j + 1)
+            return
+
         res = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
+                if grid[i][j] == "1":
+                    findborder(grid, i, j)
                     res += 1
-                    self.findBorder(grid, i, j)
 
         return res
-
-    def findBorder(self, grid, i, j):
-        if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0:
-            return
-        if grid[i][j] == '0':
-            return
-        grid[i][j] = '0'
         
-        self.findBorder(grid, i, j-1)
-        self.findBorder(grid, i, j+1)
-        self.findBorder(grid, i-1, j)
-        self.findBorder(grid, i+1, j)
-            
