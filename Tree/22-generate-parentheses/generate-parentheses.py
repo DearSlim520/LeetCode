83cb930fdfddb45ac00d1209a58e44e72f789f42
Slim <50854083+DearSlim520@users.noreply.github.com>
2026-05-02 23:08:57 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (73.87%) - LeetSync

---
+++ Tree/22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        def backtrack(track: str, preCnt: int, postCnt: int):
+            if len(track) == n * 2 and preCnt == postCnt:
+                res.append(track[:])
+                return
+            if len(track) == n * 2:
+                return
+            if postCnt > preCnt:
+                return
+            
+            for cur in ['(', ')']:
+                if cur == '(' and preCnt < n:
+                    backtrack(track + cur, preCnt + 1, postCnt)
+                elif cur == ')' and preCnt > 0:
+                    backtrack(track + cur, preCnt, postCnt + 1)
+            return
+
+        res = []
+        backtrack('', 0, 0)
+        return res
